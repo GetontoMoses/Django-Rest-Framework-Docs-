77512dae832d67b0daa5e423ec518da5ee7e35ea
Getonto3 <getontomoses18@gmail.com>
2023-12-30 22:05:12 +0300

    added the apps to the settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -33,6 +33,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #project apps
+    'rest_framework',
+    'quickstart',
 ]
 
 MIDDLEWARE = [
