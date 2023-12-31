32ffe57ebd77a626414dbf04cf2a87aeeee38d9c
Getonto3 <getontomoses18@gmail.com>
2023-12-31 22:46:15 +0300

    views for the users

--- quickstart/views.py
+++ quickstart/views.py
@@ -1,6 +1,18 @@
 from quickstart.models import Snippet
 from quickstart.serializers import SnippetSerializer
 from rest_framework import generics
+from quickstart.serializers import UserSerializer
+from django.contrib.auth.models import User
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
