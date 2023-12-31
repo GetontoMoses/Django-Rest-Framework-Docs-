d2476f55697fa9ccbd88e8201a89e2e1b937695b
Getonto3 <getontomoses18@gmail.com>
2023-12-31 21:15:16 +0300

    modifying requests and responses

--- quickstart/urls.py
+++ quickstart/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from quickstart import views
 
 urlpatterns = [
     path('quickstart/', views.snippet_list),
     path('quickstart/<int:pk>/', views.snippet_detail),
 ]
+urlpatterns = format_suffix_patterns(urlpatterns)
