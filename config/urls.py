3200e25061ae34a2198da8d2c76f866f0d438784
Getonto3 <getontomoses18@gmail.com>
2024-01-01 02:02:17 +0300

    Object level permissions

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('quickstart.urls')),
-]
-urlpatterns += [
     path('api-auth/', include('rest_framework.urls')),
 ]
--- quickstart/views.py
+++ quickstart/views.py
@@ -4,6 +4,7 @@ from rest_framework import generics
 from quickstart.serializers import UserSerializer
 from django.contrib.auth.models import User
 from rest_framework import permissions
+from quickstart.permissions import IsOwnerOrReadOnly
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
@@ -26,4 +27,5 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                      IsOwnerOrReadOnly]
